ee6e126059738ef22dc27ada7fb4a462ab144e5e
yerelkarne <99020789+yerelkarne@users.noreply.github.com>
2026-01-14 01:19:06 +0300

    Add Material dependency for XML theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation("com.google.android.gms:play-services-ads:23.1.0")
     implementation("androidx.core:core-splashscreen:1.0.1")
